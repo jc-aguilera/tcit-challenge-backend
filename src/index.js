c6a4d5b81e9c792ff6496e99d059e764c5dad64a
Juan Carlos Aguilera <jcaguile@uc.cl>
2021-01-23 19:29:50 -0300

    CORS origin

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 import models, { sequelize } from './models';
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
